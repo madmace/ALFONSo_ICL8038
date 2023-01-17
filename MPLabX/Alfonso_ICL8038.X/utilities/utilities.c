b5263661321ed510901d17b06a15c7250be21b0c
Emiliano Mazza <e.mazza@sirti.it>
2023-01-17 12:12:17 +0100

    General purpose conversion utilities

---
+++ MPLabX/Alfonso_ICL8038.X/utilities/utilities.c
@@ -0,0 +1,95 @@
+/*******************************************************************************
+
+Author : Emiliano Mazza
+Version : 1.0
+Created on Date : 01/03/2016
+Last update     : 18/03/2016
+
+CopyRight 2006-2015 all rights are reserved
+
+THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
+WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHOR SHALL NOT,
+IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+
+This Header contains local definition for main application.
+
+*******************************************************************************/
+
+#include "utilities.h"
+
+// conv_int16_to_string Implementation
+int8_t conv_int16_to_string (uint8_t * buffer, int16_t value) {
+
+    uint8_t new_digit = 0;
+    uint8_t digit_counter = 0;
+    // he flag is set if you should not ignore the following digits
+    // 0 ignore the digit
+    // 1 do not ignore the digit
+    uint8_t flag = 0;
+    
+    if (value < 0) {
+        buffer[digit_counter] = '-'; 
+        value = value * (-1);
+        digit_counter++;
+    } else {
+//        buffer[digit_counter]= '+'; 
+//        digit_counter++;
+    }
+    
+    // MSB
+    new_digit = (uint8_t) (value / 10000);
+    // Adding 48 it will convert the integer in char
+    if (new_digit != 0){
+        buffer[digit_counter]= new_digit + 48; 
+        value = value - (new_digit * 10000);
+        flag = 1;
+        digit_counter++;
+    }  
+    
+    new_digit = (uint8_t) (value / 1000);
+    if (new_digit != 0 || flag == 1){
+        buffer[digit_counter]= new_digit + 48;
+        value = value - (new_digit * 1000);
+        flag = 1;
+        digit_counter++;
+    }
+
+    new_digit = (uint8_t) (value / 100);
+    if (new_digit != 0 || flag == 1){
+        buffer[digit_counter]= new_digit + 48;
+        value = value - (new_digit * 100);
+        flag = 1;
+        digit_counter++;
+    }
+        
+    new_digit = (uint8_t) (value / 10);
+    if (new_digit != 0 || flag == 1){
+        buffer[digit_counter]= new_digit + 48;
+        value = value - (new_digit * 10);
+        flag = 1;
+        digit_counter++;
+    }
+    
+    // LSB
+    new_digit = (uint8_t)value;
+    buffer[digit_counter]= new_digit + 48;
+    digit_counter++;
+    
+	buffer[digit_counter]= '\0';
+    
+	return (0); 
+}
+
+// get_string_length Implementation
+uint8_t get_string_length (uint8_t * buffer){
+    uint8_t counter = 0;
+    
+    while (buffer[counter] != '\0'){        
+        counter++;
+    }
+    
+    return(counter); 
+}
---
+++ MPLabX/Alfonso_ICL8038.X/utilities/utilities.h
@@ -0,0 +1,52 @@
+/*******************************************************************************
+
+Author : Emiliano Mazza
+Version : 1.0
+Created on Date : 01/03/2016
+Last update     : 18/03/2016
+
+CopyRight 2006-2015 all rights are reserved
+
+THIS SOFTWARE IS PROVIDED IN AN "AS IS" CONDITION. NO WARRANTIES,
+WHETHER EXPRESS, IMPLIED OR STATUTORY, INCLUDING, BUT NOT LIMITED
+TO, IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+PARTICULAR PURPOSE APPLY TO THIS SOFTWARE. THE AUTHOR SHALL NOT,
+IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL OR
+CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
+
+*******************************************************************************/
+
+// This is a guard condition so that contents of this file are not included
+// more than once.
+#ifndef UTILITIES_H
+#define	UTILITIES_H
+
+#include <xc.h>
+
+/**
+ * This function converts signed integer to a string of chars.
+ * The returned string is always the shortest possible one containing the value.
+ * Digit 0 contains - sign if the number is negative. The sign + is not shown.
+ *
+ * @param buffer Pointer to the array that will contain the string.
+ *
+ * @param value Integer value to convert
+ * 
+ * @return error 0: Conversion OK
+ *              -1: Overflow - The number is too big  
+ */
+int8_t conv_int16_to_string (uint8_t * buffer, int16_t value);
+
+/**
+ * This function returns the string length, marked with the '\0' character
+ * The \0 character is not counted.
+ *
+ * @param buffer Pointer to the string that contains the char array ending with '\0'
+ * 
+ * @return value String length 
+ *      
+ */
+uint8_t get_string_length (uint8_t * buffer);
+
+#endif	/* UTILITIES_H */
+
